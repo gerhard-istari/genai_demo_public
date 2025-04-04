36edcef2e1eedeec951e06c518eb61f021678a9b
Gerhard Stoeckel <171275975+gerhard-istari@users.noreply.github.com>
2025-04-04 11:56:33 -0400

    Re-enabled Cameo requirements extraction

--- components/extract_requirements.py
+++ components/extract_requirements.py
@@ -7,18 +7,18 @@ from shared.constants import CAMEO_MODEL_ID, REQ_FILE_NAME
 
 
 def extract_requirements(client: Client):
-  #print('Starting Cameo model extraction ...')
-  #job = client.add_job(model_id = CAMEO_MODEL_ID,
-  #                     function = '@istari:extract',
-  #                     tool_name = 'dassault_cameo')
-  #print(f"Job submitted with ID: {job.id}")
-  #
-  #wait_for_job(job)
-  #print(f"Job Complete [{job.status.name}]")
+  print('Starting Cameo model extraction ...')
+  job = client.add_job(model_id = CAMEO_MODEL_ID,
+                       function = '@istari:extract',
+                       tool_name = 'dassault_cameo')
+  print(f"Job submitted with ID: {job.id}")
+  
+  wait_for_job(job)
+  print(f"Job Complete [{job.status.name}]")
 
   print("Downloading requirements artifact ...")
-  #download_artifact(CAMEO_MODEL_ID,
-  #                  REQ_FILE_NAME)
+  download_artifact(CAMEO_MODEL_ID,
+                    REQ_FILE_NAME)
   print('Requirements artifact downloaded')
 
 
